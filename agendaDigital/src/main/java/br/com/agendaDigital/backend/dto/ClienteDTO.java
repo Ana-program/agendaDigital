379d6f9100f315728e6edaf0b5d0605248e2004b
Ana Paula Ribeiro <ana.sed26@hotmail.com>
2025-09-21 22:39:52 -0300

    Cria classe ClienteDTO

---
+++ agendaDigital/src/main/java/br/com/agendaDigital/backend/dto/ClienteDTO.java
@@ -0,0 +1,21 @@
+package br.com.agendaDigital.backend.dto;
+
+import jakarta.persistence.Column;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class ClienteDTO {
+
+    private String nome;
+    private String cpf;
+    private String endereco;
+
+    @Column(name = "data_nascimento")
+    private LocalDate dataNascimento;
+}
