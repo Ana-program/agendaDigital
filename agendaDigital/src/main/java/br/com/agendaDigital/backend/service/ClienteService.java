9880f0e81e80e58ac3bc9a5c9d52dbb9d8847d16
Ana Paula Ribeiro <ana.sed26@hotmail.com>
2025-09-22 00:50:50 -0300

    Adiciona métodos updateCliente, deleteCliente, findAllCliente e findClienteByNomeOrCpf

--- agendaDigital/src/main/java/br/com/agendaDigital/backend/service/ClienteService.java
+++ agendaDigital/src/main/java/br/com/agendaDigital/backend/service/ClienteService.java
@@ -2,10 +2,13 @@ package br.com.agendaDigital.backend.service;
 
 import br.com.agendaDigital.backend.dto.ClienteDTO;
 import br.com.agendaDigital.backend.entity.Cliente;
+import br.com.agendaDigital.backend.exception.ClienteException;
 import br.com.agendaDigital.backend.mapper.ClienteMapper;
+import br.com.agendaDigital.backend.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import br.com.agendaDigital.backend.repository.ClienteRepository;
+
+import java.util.List;
 
 @Service
 public class ClienteService {
@@ -21,5 +24,28 @@ public class ClienteService {
        repository.save(cliente);
     }
 
+    public Cliente updateCliente(Integer id, ClienteDTO clienteDTO) {
+        Cliente cliente = repository.findById(id).orElseThrow(() -> new ClienteException("Cliente não encontrado"));
+        clienteMapper.updateCliente(clienteDTO, cliente);
+        return repository.save(cliente);
+    }
+
+    public void deleteCliente(Integer id) {
+        repository.deleteById(id);
+    }
+
+    public List<Cliente> findAllCliente() {
+        return repository.findAll();
+    }
+
+    public List<Cliente> findClienteByNomeOrCpf(String nome, String cpf) {
+        if (cpf != null && !cpf.isBlank()){
+            return repository.findByCpf(cpf);
+        }
+        if (nome != null && !nome.isBlank()){
+            return repository.findByNome(nome);
+        }
+        return repository.findAll();
+    }
 
 }
