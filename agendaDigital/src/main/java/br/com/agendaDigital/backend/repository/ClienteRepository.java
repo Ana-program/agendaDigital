b4d304163289782f126b8d82c75af53fbbf28f4c
Ana Paula Ribeiro <ana.sed26@hotmail.com>
2025-09-22 00:48:56 -0300

    Adiciona métodos findByCpf e findByNome

--- agendaDigital/src/main/java/br/com/agendaDigital/backend/repository/ClienteRepository.java
+++ agendaDigital/src/main/java/br/com/agendaDigital/backend/repository/ClienteRepository.java
@@ -3,5 +3,12 @@ package br.com.agendaDigital.backend.repository;
 import br.com.agendaDigital.backend.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+    List<Cliente> findByCpf(String cpf);
+
+    List<Cliente> findByNome(String nome);
+
 }
