016a02d9c9f6082fd47890623e3fdf134d9bca54
Ana Paula Ribeiro <ana.sed26@hotmail.com>
2025-09-22 00:50:17 -0300

    Adiciona métodos updateCliente, deleteCliente, findAllCliente e findCliente

--- agendaDigital/src/main/java/br/com/agendaDigital/backend/controller/ClienteController.java
+++ agendaDigital/src/main/java/br/com/agendaDigital/backend/controller/ClienteController.java
@@ -1,14 +1,14 @@
 package br.com.agendaDigital.backend.controller;
 
 import br.com.agendaDigital.backend.dto.ClienteDTO;
+import br.com.agendaDigital.backend.entity.Cliente;
+import br.com.agendaDigital.backend.service.ClienteService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import br.com.agendaDigital.backend.service.ClienteService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/clientes")
@@ -21,6 +21,33 @@ public class ClienteController {
     public ResponseEntity<Void> criarCliente(@Valid @RequestBody ClienteDTO clienteDTO){
         service.registrarCliente(clienteDTO);
         return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> updateCliente(@PathVariable Integer id, @RequestBody ClienteDTO clienteDTO){
+        return ResponseEntity.ok(service.updateCliente(id, clienteDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCliente(@PathVariable Integer id){
+        service.deleteCliente(id);
+    }
+
+    @GetMapping("/findAll")
+    public ResponseEntity<List<Cliente>> findAllCliente(){
+        return ResponseEntity.ok( service.findAllCliente());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Cliente>> findCliente(@RequestParam(required = false) String nome,
+                                                     @RequestParam(required = false) String cpf){
+        List<Cliente> result = service.findClienteByNomeOrCpf(nome, cpf);
+
+        if (result.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(result);
     }
 
 }
