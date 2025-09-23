23c5b32391c0ddddea0c529fd4a4fb9a72faec7b
Ana Paula Ribeiro <ana.sed26@hotmail.com>
2025-09-22 23:30:19 -0300

    Adiciona método findByClienteId na interface ContatoRepository

--- agendaDigital/src/main/java/br/com/agendaDigital/backend/repository/ContatoRepository.java
+++ agendaDigital/src/main/java/br/com/agendaDigital/backend/repository/ContatoRepository.java
@@ -3,5 +3,9 @@ package br.com.agendaDigital.backend.repository;
 import br.com.agendaDigital.backend.entity.Contato;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ContatoRepository extends JpaRepository<Contato, Integer> {
+
+    List<Contato> findByClienteId(Integer clienteId);
 }
