bde0b78ab551b3c605170ef05540bd608a4334df
Ana Paula Ribeiro <ana.sed26@hotmail.com>
2025-09-22 23:17:33 -0300

    Atualiza classe ClienteMapper

--- agendaDigital/src/main/java/br/com/agendaDigital/backend/mapper/ClienteMapper.java
+++ agendaDigital/src/main/java/br/com/agendaDigital/backend/mapper/ClienteMapper.java
@@ -10,20 +10,9 @@ import java.util.Optional;
 @Component
 public class ClienteMapper {
 
-    public Cliente toEntity(ClienteDTO clienteDTO) {
-        Cliente cliente = new Cliente();
-        cliente.setNome(clienteDTO.getNome());
-        cliente.setCpf(clienteDTO.getCpf());
-        cliente.setEndereco(clienteDTO.getEndereco());
-        cliente.setDataNascimento(clienteDTO.getDataNascimento());
-        cliente.setContatos(new ArrayList<>());
-        return cliente;
-    }
-
     public void updateCliente(ClienteDTO clienteDTO, Cliente cliente) {
         Optional.ofNullable(clienteDTO.getNome()).ifPresent(cliente::setNome);
         Optional.ofNullable(clienteDTO.getEndereco()).ifPresent(cliente::setEndereco);
         Optional.ofNullable(clienteDTO.getDataNascimento()).ifPresent(cliente::setDataNascimento);
-        Optional.ofNullable(clienteDTO.getCpf()).ifPresent(cliente::setCpf);
     }
 }
